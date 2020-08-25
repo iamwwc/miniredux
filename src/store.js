e605be58010a4efd95848b1d3fd3e5dcac589b74
wuweichao <wuweichao.1@bytedance.com>
2020-08-25 10:33:53 +0800

    开发模式下redux将会在开发正工具redux插件中显示

--- src/store.js
+++ src/store.js
@@ -1,4 +1,10 @@
 import { createStore } from 'redux'
 import reducers from './reducers'
+let enhancer = undefined
+if (process.env.NODE_ENV === 'development'){
+    enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({
+        trace: true
+    })
+}
 
-export const store = createStore(reducers)
+export const store = createStore(reducers,enhancer)
